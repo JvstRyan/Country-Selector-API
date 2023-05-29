f79792faf7e516882c2ddbcb8552edf76ef7805c
Ryan Horrmann <113124846+JvstRyan@users.noreply.github.com>
2023-05-29 21:14:35 +0200

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,43 @@
+const searchButton = document.querySelector('.click-me');
+
+
+const getCountryData = function(country) {
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+
+///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v2/name/${country}`);
+request.send();
+
+
+request.addEventListener('load', () => {
+    console.log(request.responseText);
+    const [data] = JSON.parse(request.responseText)
+    console.log(data)
+
+    const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+});
+}
+
+//https://countries-api-836d.onrender.com/countries/
+
+searchButton.addEventListener('click', () => {
+    const inputValue = document.querySelector('.country-name').value;
+    getCountryData(inputValue);
+})
+
